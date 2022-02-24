7e90e87d19b0f68b3eb8063a10e87198b86bc927
Gaby <gabriela.cisneros@outlook.com>
2022-02-24 10:51:28 +0000

    API added

--- Ideas1.py
+++ Ideas1.py
@@ -6,5 +6,24 @@ Created on Wed Feb 23 16:56:41 2022
 """
 
 #Hola Gaby
-#Estas son ideas
-#Quien acabo la carrera
+#First trial to have F1 data
+
+
+
+##############################################################################
+#https://ergast.com/mrd/
+##############################################################################
+
+import requests
+
+#Data from Results Ergast API
+#Documentation https://ergast.com/mrd/methods/results/
+response=requests.get("http://ergast.com/api/f1/2021/1/results.json")
+
+print(response.status_code) #200 ok
+
+response.json() #json response
+
+
+#To continue
+#https://ergast.com/mrd/methods/results/
