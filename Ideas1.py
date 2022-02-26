2b25595b650297e6492fce2adb93264cefbd90f1
Gaby <gabriela.cisneros@outlook.com>
2022-02-26 22:36:08 +0000

    Laps added

--- Ideas1.py
+++ Ideas1.py
@@ -139,6 +139,59 @@ QualifyingDF.info()
 
 
 
+### Laps times per race, per driver
+
+LapsDF=pd.DataFrame()
+
+for year in range(2003,2022): #Available from 2003
+    for race in range(1,30): #The maximum number of races was in 2021 with 22 races
+        for lap in range(1,100): #The maximum number of laps per race is 76
+            url="http://ergast.com/api/f1/"+str(year)+"/"+str(race)+"/laps/"+str(lap)+".json" #modifying the url
+            Results=requests.get(url) #Request from API
+            Results=Results.json() #Results in json format
+            Races=Results['MRData']['RaceTable']["Races"] #subset of desired features
+            
+            #Each season has a different number of races per year
+            if Races!=[]: #if the year and number of race exist
+                Races=Results['MRData']['RaceTable']["Races"][0] #subset of desired features
+                print("Season ",year,"- Race ",race,": The race was held.")
+                #General information about the race and season
+                df1 = pd.json_normalize(Races)
+                df1["LapNumber"]=lap
+                df1=df1.drop(['Laps', 'url'], axis=1)
+                
+                #Specific information of the lap
+                df2 = pd.json_normalize(Races["Laps"],record_path=['Timings'],
+                                       meta=["number"])
+                df2["number"] = pd.to_numeric(df2["number"])
+                df2=df2.rename(columns={"number": "LapNumber"})
+                
+                Laps=pd.merge(df1, df2,how="right",on="LapNumber")
+                LapsDF = pd.concat([LapsDF, Laps], axis=0)
+            elif Races==[]: #if this number of race was not held that year
+                print("Season ",year,"- Race ",race,": The race was NOT held.")
+                continue
+
+
+
+
+
+
+
+#2 The last race url has a different format:
+url="http://ergast.com/api/f1/current/last/qualifying.json" #modifying the url
+Results=requests.get(url) #Request from API
+Results=Results.json() #Results in json format
+Races=Results['MRData']['RaceTable']["Races"] #subset of desired features   
+df= pd.json_normalize(Races,record_path=['QualifyingResults'],
+                       meta=["season","round",'raceName',
+                             ['Circuit','circuitId'],
+                             ['Circuit','circuitName'],
+                             ['Circuit','Location','country'],
+                             'date'])
+QualifyingDF = pd.concat([QualifyingDF, df], axis=0)
+QualifyingDF.info()
+
 
 
 
