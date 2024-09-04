e4af5da687e16336b7b0cb0b2ea09db0693a8513
GabyCisnerosV <gabriela.cisneros@outlook.com>
2024-09-04 07:40:04 +0100

    latest changes

--- API_Requests_Results_Qualifying_Laps_PitStops.py
+++ API_Requests_Results_Qualifying_Laps_PitStops.py
@@ -42,8 +42,6 @@ import pandas as pd
 #Downloading data from Races, Qualifying sessions, pit stops, laps
 ##############################################################################
 
-
-
 ##############################################################################
 ### Race Results per race per driver
 
@@ -51,8 +49,8 @@ import pandas as pd
 
 ResultsDF=pd.DataFrame()
 
-for year in range(2003,2023): #From 2003 until 2022 (to be able to use in the close future)
-    for race in range(1,30): #The maximum number of races was in 2021 with 22 races
+for year in range(2003,2025): #From 2003 until 2022 (to be able to use in the close future)
+    for race in range(1,40): #The maximum number of races was in 2021 with 22 races
         url="http://ergast.com/api/f1/"+str(year)+"/"+str(race)+"/results.json" #modifying the url
         Results=requests.get(url) #Request from API
         Results=Results.json() #Results in json format
@@ -91,109 +89,105 @@ ResultsDF = pd.concat([ResultsDF, df], axis=0)
 
 
 
-
-
 ##############################################################################
 ### Qualifying Results per race, per driver
 
-QualifyingDF=pd.DataFrame()
+# QualifyingDF=pd.DataFrame()
 
-for year in range(2003,2023): #Available from 2003
-    for race in range(1,30): #The maximum number of races was in 2021 with 22 races
-        url="http://ergast.com/api/f1/"+str(year)+"/"+str(race)+"/qualifying.json" #modifying the url
-        Results=requests.get(url) #Request from API
-        Results=Results.json() #Results in json format
-        Races=Results['MRData']['RaceTable']["Races"] #subset of desired features
+# for year in range(2003,2023): #Available from 2003
+#     for race in range(1,30): #The maximum number of races was in 2021 with 22 races
+#         url="http://ergast.com/api/f1/"+str(year)+"/"+str(race)+"/qualifying.json" #modifying the url
+#         Results=requests.get(url) #Request from API
+#         Results=Results.json() #Results in json format
+#         Races=Results['MRData']['RaceTable']["Races"] #subset of desired features
         
-        #Each season has a different number of races per year
-        if Races!=[]: #if the year and number of race exist
-            print("Season ",year,"- Race ",race,": The race was held.")
-            df= pd.json_normalize(Races,record_path=['QualifyingResults'],
-                       meta=["season","round",'raceName',
-                             ['Circuit','circuitId'],
-                             ['Circuit','circuitName'],
-                             ['Circuit','Location','country'],
-                             'date'])
-            QualifyingDF = pd.concat([QualifyingDF, df], axis=0)
-        elif Races==[]: #if this number of race was not held that year
-            print("Season ",year,"- Race ",race,": The race was NOT held.")
-            continue
+#         #Each season has a different number of races per year
+#         if Races!=[]: #if the year and number of race exist
+#             print("Season ",year,"- Race ",race,": The race was held.")
+#             df= pd.json_normalize(Races,record_path=['QualifyingResults'],
+#                        meta=["season","round",'raceName',
+#                              ['Circuit','circuitId'],
+#                              ['Circuit','circuitName'],
+#                              ['Circuit','Location','country'],
+#                              'date'])
+#             QualifyingDF = pd.concat([QualifyingDF, df], axis=0)
+#         elif Races==[]: #if this number of race was not held that year
+#             print("Season ",year,"- Race ",race,": The race was NOT held.")
+#             continue
         
 
-QualifyingDF.info()
+# QualifyingDF.info()
 
 
 
+# ##############################################################################
+# ### Laps times per race, per driver
 
+# LapsDF=pd.DataFrame()
 
-
-##############################################################################
-### Laps times per race, per driver
-
-LapsDF=pd.DataFrame()
-
-for year in range(2003,2023): #Available from 2003
-    for race in range(1,30): #The maximum number of races was in 2021 with 22 races
-        for lap in range(1,100): #The maximum number of laps per race is 76
-            url="http://ergast.com/api/f1/"+str(year)+"/"+str(race)+"/laps/"+str(lap)+".json" #modifying the url
-            Results=requests.get(url) #Request from API
-            Results=Results.json() #Results in json format
-            Races=Results['MRData']['RaceTable']["Races"] #subset of desired features
+# for year in range(2003,2023): #Available from 2003
+#     for race in range(1,30): #The maximum number of races was in 2021 with 22 races
+#         for lap in range(1,100): #The maximum number of laps per race is 76
+#             url="http://ergast.com/api/f1/"+str(year)+"/"+str(race)+"/laps/"+str(lap)+".json" #modifying the url
+#             Results=requests.get(url) #Request from API
+#             Results=Results.json() #Results in json format
+#             Races=Results['MRData']['RaceTable']["Races"] #subset of desired features
             
-            #Each season has a different number of races per year
-            if Races!=[]: #if the year and number of race exist
-                Races=Results['MRData']['RaceTable']["Races"][0] #subset of desired features
-                print("Season ",year,"- Race ",race,": The race was held.")
-                #General information about the race and season
-                df1 = pd.json_normalize(Races)
-                df1["LapNumber"]=lap
-                df1=df1.drop(['Laps', 'url'], axis=1)
+#             #Each season has a different number of races per year
+#             if Races!=[]: #if the year and number of race exist
+#                 Races=Results['MRData']['RaceTable']["Races"][0] #subset of desired features
+#                 print("Season ",year,"- Race ",race,": The race was held.")
+#                 #General information about the race and season
+#                 df1 = pd.json_normalize(Races)
+#                 df1["LapNumber"]=lap
+#                 df1=df1.drop(['Laps', 'url'], axis=1)
                 
-                #Specific information of the lap
-                df2 = pd.json_normalize(Races["Laps"],record_path=['Timings'],
-                                       meta=["number"])
-                df2["number"] = pd.to_numeric(df2["number"])
-                df2=df2.rename(columns={"number": "LapNumber"})
+#                 #Specific information of the lap
+#                 df2 = pd.json_normalize(Races["Laps"],record_path=['Timings'],
+#                                        meta=["number"])
+#                 df2["number"] = pd.to_numeric(df2["number"])
+#                 df2=df2.rename(columns={"number": "LapNumber","time":"LapDuration"})
                 
-                Laps=pd.merge(df1, df2,how="right",on="LapNumber")
-                LapsDF = pd.concat([LapsDF, Laps], axis=0)
-            elif Races==[]: #if this number of race was not held that year
-                print("Season ",year,"- Race ",race,": The race was NOT held.")
-                continue
+#                 Laps=pd.merge(df1, df2,how="right",on="LapNumber")
+#                 LapsDF = pd.concat([LapsDF, Laps], axis=0)
+#             elif Races==[]: #if this number of race was not held that year
+#                 print("Season ",year,"- Race ",race,": The race was NOT held.")
+#                 continue
 
 
+# LapsDF.info()
+# LapsDF=LapsDF.drop(["time"],axis=1)
 
 
+# ##############################################################################
+# ### Pits per race, per driver
 
-##############################################################################
-### Pits per race, per driver
+# PitsDF=pd.DataFrame()
 
-PitsDF=pd.DataFrame()
-
-for year in range(2012,2023): #Available from 2012
-    for race in range(1,30): #The maximum number of races was in 2021 with 22 races
-        url="http://ergast.com/api/f1/"+str(year)+"/"+str(race)+"/pitstops.json" #modifying the url
-        Results=requests.get(url) #Request from API
-        Results=Results.json() #Results in json format
-        Races=Results['MRData']['RaceTable']["Races"] #subset of desired features
+# for year in range(2012,2023): #Available from 2012
+#     for race in range(1,30): #The maximum number of races was in 2021 with 22 races
+#         url="http://ergast.com/api/f1/"+str(year)+"/"+str(race)+"/pitstops.json" #modifying the url
+#         Results=requests.get(url) #Request from API
+#         Results=Results.json() #Results in json format
+#         Races=Results['MRData']['RaceTable']["Races"] #subset of desired features
         
-        #Each season has a different number of races per year
-        if Races!=[]: #if the year and number of race exist
-            Races=Results['MRData']['RaceTable']["Races"][0]
-            print("Season ",year,"- Race ",race,": The race was held.")
-            df= pd.json_normalize(Races,record_path=['PitStops'],
-                       meta=["season","round",'raceName',
-                             ['Circuit','circuitId'],
-                             ['Circuit','circuitName'],
-                             ['Circuit','Location','country'],
-                             'date'])
-            PitsDF = pd.concat([PitsDF, df], axis=0)
-        elif Races==[]: #if this number of race was not held that year
-            print("Season ",year,"- Race ",race,": The race was NOT held.")
-            continue
+#         #Each season has a different number of races per year
+#         if Races!=[]: #if the year and number of race exist
+#             Races=Results['MRData']['RaceTable']["Races"][0]
+#             print("Season ",year,"- Race ",race,": The race was held.")
+#             df= pd.json_normalize(Races,record_path=['PitStops'],
+#                        meta=["season","round",'raceName',
+#                              ['Circuit','circuitId'],
+#                              ['Circuit','circuitName'],
+#                              ['Circuit','Location','country'],
+#                              'date'])
+#             PitsDF = pd.concat([PitsDF, df], axis=0)
+#         elif Races==[]: #if this number of race was not held that year
+#             print("Season ",year,"- Race ",race,": The race was NOT held.")
+#             continue
 
 
-PitsDF.info()
+# PitsDF.info()
 
 
 
@@ -203,7 +197,7 @@ PitsDF.info()
 path = 'C:/Users/gabri/Dropbox/Gaby/Proyectos/My_Portafolio/F1/Data/'
 
 ResultsDF.to_csv(path+"ResultsDF.csv")
-QualifyingDF.to_csv(path+"QualifyingDF.csv")
-LapsDF.to_csv(path+"LapsDF.csv")
-PitsDF.to_csv(path+"PitsDF.csv")
+# QualifyingDF.to_csv(path+"QualifyingDF.csv")
+# LapsDF.to_csv(path+"LapsDF.csv")
+# PitsDF.to_csv(path+"PitsDF.csv")
 
